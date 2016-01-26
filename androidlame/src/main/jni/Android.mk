22935aba0b2c2689045a87815c77bef11f8d44fe
naman14 <namandwivedi14@gmail.com>
2016-01-26 16:21:52 +0530

    update gitignore

--- androidlame/src/main/jni/Android.mk
+++ androidlame/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
     include $(CLEAR_VARS)
 
-    LOCAL_MODULE        := libmp3lame
+    LOCAL_MODULE        := androidlame
     LOCAL_SRC_FILES     := \
     ./libmp3lame/bitstream.c \
     ./libmp3lame/encoder.c \
--- androidlame/src/main/obj/local/armeabi/objs/mp3lame/libmp3lame/mpglib_interface.o.d
+++
@@ -1,2 +0,0 @@
-/Users/naman/Github/TAndroidLame/androidlame/src/main/obj/local/armeabi/objs/mp3lame/./libmp3lame/mpglib_interface.o: \
- /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/mpglib_interface.c
