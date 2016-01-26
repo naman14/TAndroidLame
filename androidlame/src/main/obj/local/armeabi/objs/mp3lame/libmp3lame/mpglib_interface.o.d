fe79c0e708f749e18549b189c485b6dc973dad0a
naman14 <namandwivedi14@gmail.com>
2016-01-26 13:22:43 +0530

    import lame and generate shared lib

---
+++ androidlame/src/main/obj/local/armeabi/objs/mp3lame/libmp3lame/mpglib_interface.o.d
@@ -0,0 +1,2 @@
+/Users/naman/Github/TAndroidLame/androidlame/src/main/obj/local/armeabi/objs/mp3lame/./libmp3lame/mpglib_interface.o: \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/mpglib_interface.c
---
+++ androidlame/src/main/obj/local/armeabi/objs/mp3lame/libmp3lame/presets.o.d
@@ -0,0 +1,149 @@
+/Users/naman/Github/TAndroidLame/androidlame/src/main/obj/local/armeabi/objs/mp3lame/./libmp3lame/presets.o: \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/presets.c \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/lame.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/machine.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/version.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/assert.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/ctype.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/endian.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/set_get.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/encoder.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/util.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/l3side.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/id3tag.h \
+ /Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/lame_global_flags.h
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/lame.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/machine.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/version.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/assert.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/ctype.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/Users/naman/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/set_get.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/encoder.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/util.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/l3side.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/id3tag.h:
+
+/Users/naman/Github/TAndroidLame/androidlame/src/main/jni/./libmp3lame/lame_global_flags.h:
