49f5c8fce008da353187c445cc1ee589defbb641
naman14 <namandwivedi14@gmail.com>
2016-01-28 22:45:44 +0530

    mp3 audio record demo

--- app/src/main/java/com/naman14/tandroidlame/Mp3AudioRecordActivity.java
+++ app/src/main/java/com/naman14/tandroidlame/Mp3AudioRecordActivity.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.naman14.androidlame.AndroidLame;
+import com.naman14.androidlame.LameBuilder;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 /**
@@ -23,27 +29,101 @@ public class Mp3AudioRecordActivity extends AppCompatActivity {
 
     String filePath = Environment.getExternalStorageDirectory() + "/testrecord.mp3";
 
+    boolean isRecording = true;
+
+    AudioRecord audioRecord;
+    AndroidLame androidLame;
+    FileOutputStream outputStream;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_record);
 
+        Button start = (Button) findViewById(R.id.startRecording);
+        Button stop = (Button) findViewById(R.id.stopRecording);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecording();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRecording = false;
+            }
+        });
+
+    }
+
+    private void startRecording() {
+
         minBuffer = AudioRecord.getMinBufferSize(inSamplerate, AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT);
 
-        AudioRecord audioRecord = new AudioRecord(
+        audioRecord = new AudioRecord(
                 MediaRecorder.AudioSource.MIC, inSamplerate,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT, minBuffer * 2);
 
         short[] buffer = new short[inSamplerate * 2];
+
+        // 'mp3buf' should be at least 7200 bytes long
+        // to hold all possible emitted data.
         byte[] mp3buffer = new byte[(int) (7200 + buffer.length * 2 * 1.25)];
 
         try {
-            FileOutputStream outputStream = new FileOutputStream(new File(filePath));
+            outputStream = new FileOutputStream(new File(filePath));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        androidLame = new LameBuilder()
+                .setInSampleRate(inSamplerate)
+                .setOutChannels(1)
+                .build();
+
+        audioRecord.startRecording();
+
+        int bytesRead = 0;
+
+        while (isRecording) {
+            bytesRead = audioRecord.read(buffer, 0, minBuffer);
+
+            if (bytesRead > 0) {
+
+                int bytesEncoded = androidLame.encodeBufferInterLeaved(buffer, bytesRead, mp3buffer);
+
+                if (bytesEncoded > 0) {
+                    try {
+                        outputStream.write(mp3buffer, 0, bytesEncoded);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+        int outputMp3buf = androidLame.flush(mp3buffer);
+
+        if (outputMp3buf > 0) {
+            try {
+                outputStream.write(mp3buffer, 0, outputMp3buf);
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        audioRecord.stop();
+        audioRecord.release();
+
+        androidLame.close();
+
+        isRecording = false;
     }
 }
